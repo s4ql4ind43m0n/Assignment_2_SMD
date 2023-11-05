87d693031d8d0424cabbaf7543fff0a2829b926e
s4ql4ind43m0n <131148557+s4ql4ind43m0n@users.noreply.github.com>
2023-11-05 13:26:36 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/composable1/MainActivity.kt
+++ app/src/main/java/com/example/composable1/MainActivity.kt
@@ -1,37 +1,168 @@
-package com.example.composable1
+package com.example.assignment2
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composable1.ui.theme.Composable1Theme
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.assignment2.ui.theme.Assignment2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text("Hello")
-                Text("World")
+            Assignment2Theme {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    ImageLayout()
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun ImageLayout() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.backgroundwa),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        TopBar()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 100.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "20",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                )
+                Text(
+                    text = "°C",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                )
+
+            }
+            Text(
+                text = "HUMID",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            )
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)// Make the image fill 50% of the screen width
+            )
+            Text(
+                text = "13 Feb, WED",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            )
+            Text(
+                text = "2:00PM°",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            )
 
         }
+        bottomNavDays(modifier = Modifier.align(Alignment.BottomCenter))
+
+    }
+
+
+}
+
+@Composable
+fun TopBar(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+    ) {
+        Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+        Text(
+            text = "Gotham City",
+            style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+        )
+        Icon(imageVector = Icons.Default.Search, contentDescription = null)
+        Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+    }
+}
+
+@Composable
+fun bottomNavDays(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(color = Color(47, 17, 3), shape = MaterialTheme.shapes.medium)
+    ) {
+        Text(text = "Mon", style = TextStyle(color = Color.White, fontSize = 18.sp))
+        Text(text = "Tue", style = TextStyle(color = Color.White, fontSize = 18.sp))
+        Text(text = "Wed", style = TextStyle(color = Color(254,146,6), fontSize = 18.sp))
+        Text(text = "Thu", style = TextStyle(color = Color.White, fontSize = 18.sp))
+        Text(text = "Fri", style = TextStyle(color = Color.White, fontSize = 18.sp))
     }
 }
